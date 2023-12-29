0e744f7c0fc3f250aab2f5dcccca90d161e2e40d
DucTH <ducth@rikkeisoft.com>
2023-12-29 09:01:40 +0700

    update font

--- src/pages/layout/SignUpIn.js
+++ src/pages/layout/SignUpIn.js
@@ -15,7 +15,6 @@ export default function App() {
     "container " + (type === "signUp" ? "right-panel-active" : "");
   return (
     <div className="App">
-      <h2>Sign in/up Form</h2>
       <div className={containerClass} id="container">
         <SignUpForm />
         <SignInForm />
@@ -31,7 +30,7 @@ export default function App() {
                 id="signIn"
                 onClick={() => handleOnClick("signIn")}
               >
-                Sign In
+                Log In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
